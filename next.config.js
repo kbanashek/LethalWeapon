fcd09f312853af86fb49a9ad1325dd8563026293
Kyle Banashek <kylebanashek@yahoo.com>
2025-03-24 12:34:02 -0400

    build fixes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  output: 'export',
   images: {
     unoptimized: true,
   },
+  // This is needed for the export to work properly in Next.js 14+
+  distDir: 'out',
 }
 
 module.exports = nextConfig
